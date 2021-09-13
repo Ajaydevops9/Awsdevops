5d813688d1ce74db41e5d9742afab2337a155e9b
Cloud User <ec2-user@ip-172-31-19-61.us-east-2.compute.internal>
2021-09-13 05:19:17 +0000

    part of new project

---
+++ Login.java
@@ -0,0 +1,13 @@
+   kdnkdnRunning
+Public IPv4 DNS
+ ec2-18-118-104-176.us-east-2.compute.amazonaws.com | open address 
+Private IPv4 DNS
+ ip-172-31-19-61.us-east-2.compute.internal
+Instance type
+t2.micro
+Elastic IP addresses
+–
+VPC ID
+ vpc-fa4f2791 
+AWS Compute Optimizer finding
+Opt-in to AWS Compute Optimizer for
